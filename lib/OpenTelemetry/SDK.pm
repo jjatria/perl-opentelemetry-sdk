8f4e340b89d5c9a144cccf2f76e4226fc311cfc5
José Joaquín Atria <jjatria@gmail.com>
2023-07-25 23:01:04 +0100

    Start fleshing out configurator entrypoint

--- lib/OpenTelemetry/SDK.pm
+++ lib/OpenTelemetry/SDK.pm
@@ -5,10 +5,23 @@ our $VERSION = '0.001';
 
 use strict;
 use warnings;
-use experimental 'signatures';
+use experimental qw( try signatures );
 
-sub configure ($) {
-    ...
+use OpenTelemetry;
+use OpenTelemetry::SDK::Configurator;
+
+sub configure ( $, $block ) {
+    try {
+        my $configurator = OpenTelemetry::SDK::Configurator->new;
+        $configurator->$block;
+        $configurator->configure;
+    }
+    catch ($e) {
+        OpenTelemetry->handle_error(
+            exception => $e,
+            message   => "Unexpected configuration error: $e"
+        );
+    }
 }
 
 1;
