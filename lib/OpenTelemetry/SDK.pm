bd5f78490074fd23d9637f2c97938f96bfd2c427
José Joaquín Atria <jjatria@gmail.com>
2023-08-10 11:05:29 +0100

    Simplify SDK configuration

--- lib/OpenTelemetry/SDK.pm
+++ lib/OpenTelemetry/SDK.pm
@@ -5,28 +5,113 @@ our $VERSION = '0.001';
 
 use strict;
 use warnings;
-use experimental qw( try signatures );
+use experimental qw( try signatures lexical_subs );
+use feature 'state';
 
+use Module::Runtime 'require_module';
+use OpenTelemetry::Common 'config';
+use OpenTelemetry::Propagator::Composite;
+use OpenTelemetry::SDK::Trace::TracerProvider;
 use OpenTelemetry;
-use OpenTelemetry::SDK::Configurator;
 
-# Experimental
-sub import ( $class, @options ) {
-    while ( my $option = shift @options ) {
-        if ( $option eq '-configure' ) {
-            $class->configure;
+my sub configure_propagators {
+    my $logger = OpenTelemetry->logger;
+
+    state %map = (
+        b3           => 'B3',
+        b3multi      => 'B3::Multi',
+        baggage      => 'Baggage',
+        jaeger       => 'Jaeger',
+        none         => 'None',
+        ottrace      => 'OTTrace',
+        tracecontext => 'TraceContext',
+        xray         => 'XRay',
+    );
+
+    my @names = split ',',
+        ( config('PROPAGATORS') // 'tracecontext,baggage' ) =~ s/\s//gr;
+
+    my ( %seen, @propagators );
+
+    for my $name ( @names ) {
+        my $suffix = $map{$name} // do {
+            $logger->warnf("Unknown propagator '%s' cannot be configured", $name);
+            $map{none};
+        };
+
+        next if $seen{$suffix}++;
+
+        my $class = 'OpenTelemetry::Propagator::' . $suffix;
+
+        try {
+            Module::Runtime::require_module $class;
+            push @propagators, $class->new;
+        }
+        catch ($e) {
+            $logger->warnf("Error configuring '%s' propagator: %s", $name, $e);
+        }
+    }
+
+    OpenTelemetry->propagation(
+        OpenTelemetry::Propagator::Composite->new(@propagators),
+    );
+}
+
+my sub configure_span_processors {
+    my $logger = OpenTelemetry->logger;
+
+    state %map = (
+        jaeger  => 'Jaeger',
+        otlp    => 'OTLP',
+        zipkin  => 'Zipkin',
+        console => 'Console',
+    );
+
+    my @names = split ',',
+        ( config('TRACES_EXPORTER') // 'otlp' ) =~ s/\s//gr;
+
+    my $tracer_provider = OpenTelemetry::SDK::Trace::TracerProvider->new;
+
+    my %seen;
+    for my $name ( @names ) {
+        next if $name eq 'none';
+
+        unless ( $map{$name} ) {
+            $logger->warnf("Unknown exporter '%s' cannot be configured", $name);
             next;
         }
 
-        die "Unknown import option: $option";
+        next if $seen{ $map{$name} }++;
+
+        my $exporter  = 'OpenTelemetry::SDK::Trace::Span::Exporter::' . $map{$name};
+        my $processor = 'OpenTelemetry::SDK::Trace::Span::Processor::'
+            . ( $name eq 'console' ? 'Simple' : 'Batch' );
+
+        try {
+            Module::Runtime::require_module $exporter;
+            Module::Runtime::require_module $processor;
+
+            $tracer_provider->add_span_processor(
+                $processor->new( exporter => $exporter->new )
+            );
+        }
+        catch ($e) {
+            $logger->warnf("Error configuring '%s' span processor: %s", $name, $e);
+        }
     }
+
+    return $tracer_provider;
 }
 
-sub configure ( $, $block = undef ) {
+sub import ( $class ) {
     try {
-        my $configurator = OpenTelemetry::SDK::Configurator->new;
-        $configurator->$block if $block;
-        $configurator->configure;
+        # TODO: logger
+        # TODO: error_handler
+
+        configure_propagators();
+
+        my $provider = configure_span_processors();
+        OpenTelemetry->tracer_provider($provider);
     }
     catch ($e) {
         OpenTelemetry->handle_error(
