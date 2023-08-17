df1c7fd0f909e294bdcfeedc80df8085cdf2f623
José Joaquín Atria <jjatria@gmail.com>
2023-08-17 23:45:17 +0100

    Use BUILDARGS instead of ADJUSTPARAMS in Resource

--- lib/OpenTelemetry/SDK/Resource.pm
+++ lib/OpenTelemetry/SDK/Resource.pm
@@ -15,25 +15,31 @@ class OpenTelemetry::SDK::Resource :does(OpenTelemetry::Attributes) {
 
     field $schema_url :param :reader //= '';
 
-    ADJUSTPARAMS ( $params ) {
-        my %new = map split( '=', $_, 2 ),
+    sub BUILDARGS ( $class, %args ) {
+        my %env = map split( '=', $_, 2 ),
             split ',', config('RESOURCE_ATTRIBUTES') // '';
 
-        # TODO: Should these be split / moved somewhere else?
-        # How are they overidden?
-        $new{'service.name'}            = config('SERVICE_NAME') // 'unknown_service';
-        $new{'telemetry.sdk.name'}      = 'opentelemetry';
-        $new{'telemetry.sdk.language'}  = 'perl';
-        $new{'telemetry.sdk.version'}   = $OpenTelemetry::SDK::VERSION;
-        $new{'process.pid'}             = $$;
-        $new{'process.command'}         = $0;
-        $new{'process.executable.path'} = $^X;
-        $new{'process.command_args'}    = [ @ARGV ],
-        $new{'process.executable.name'} = basename $^X;
-        $new{'process.runtime.name'}    = 'perl';
-        $new{'process.runtime.version'} = "$^V";
+        $args{attributes} = {
+            # TODO: Should these be split / moved somewhere else?
+            # How are they overidden?
+            'service.name'            => config('SERVICE_NAME') // 'unknown_service',
+            'telemetry.sdk.name'      => 'opentelemetry',
+            'telemetry.sdk.language'  => 'perl',
+            'telemetry.sdk.version'   => $OpenTelemetry::SDK::VERSION,
+            'process.pid'             => $$,
+            'process.command'         => $0,
+            'process.executable.path' => $^X,
+            'process.command_args'    => [ @ARGV ],
+            'process.executable.name' => basename($^X),
+            'process.runtime.name'    => 'perl',
+            'process.runtime.version' => "$^V",
 
-        $self->_set_attribute(%new);
+            %env,
+
+            %{ $args{attributes} // {} },
+        };
+
+        %args;
     }
 
     method merge ( $new ) {
