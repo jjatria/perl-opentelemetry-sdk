f2350a2e36317042f3fed1f485fd4e19e8fb9b59
José Joaquín Atria <jjatria@gmail.com>
2023-11-01 23:21:00 +0000

    Bump version

--- lib/OpenTelemetry/SDK/Exporter/Console.pm
+++ lib/OpenTelemetry/SDK/Exporter/Console.pm
@@ -3,7 +3,7 @@ use Object::Pad ':experimental(init_expr)';
 
 package OpenTelemetry::SDK::Exporter::Console;
 
-our $VERSION = '0.011';
+our $VERSION = '0.012';
 
 class OpenTelemetry::SDK::Exporter::Console
     :does(OpenTelemetry::Exporter)
