79718c90b32eb42328373cd1e5d8264e812b089e
José Joaquín Atria <jjatria@gmail.com>
2023-11-23 19:31:40 +0000

    Bump version

--- lib/OpenTelemetry/SDK/Trace/Span/Processor/Batch.pm
+++ lib/OpenTelemetry/SDK/Trace/Span/Processor/Batch.pm
@@ -3,7 +3,7 @@ use Object::Pad ':experimental(init_expr)';
 
 package OpenTelemetry::SDK::Trace::Span::Processor::Batch;
 
-our $VERSION = '0.019';
+our $VERSION = '0.020';
 
 class OpenTelemetry::SDK::Trace::Span::Processor::Batch
     :does(OpenTelemetry::Trace::Span::Processor)
--- lib/OpenTelemetry/SDK/Trace/TracerProvider.pm
+++ lib/OpenTelemetry/SDK/Trace/TracerProvider.pm
@@ -3,7 +3,7 @@ use Object::Pad ':experimental(init_expr)';
 
 package OpenTelemetry::SDK::Trace::TracerProvider;
 
-our $VERSION = '0.019';
+our $VERSION = '0.020';
 
 use OpenTelemetry;
 
