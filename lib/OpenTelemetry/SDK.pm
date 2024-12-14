4d7fcd3b9e67ddfbcb0ceeb975892e0c2331073f
José Joaquín Atria <jjatria@cpan.org>
2024-12-14 22:57:52 +0000

    Die on errors during import
    The [specification] states that
    
    > OpenTelemetry implementations MUST NOT throw unhandled exceptions
    > at run time.
    
    but it also says that
    
    > The [...] SDK MAY _fail fast_ and cause the application to fail on
    > initialization, e.g. because of a bad user config or environment,
    > but MUST NOT cause the application to fail later at run time, e.g.
    > due to dynamic config settings received from the Collector.
    
    Before this change, the SDK was not "failing fast" for the sake of
    application stability. However, this could result in setups that were
    misconfigured but kept running, giving the user the incorrect impression
    that things had gone according to plan. This made debugging harder.
    
    The situation was made worse because before 56d6689b66cec4f8d1950750ed044f872e561ce1
    (which added the OTLP exporter as a dependency of the SDK) because in
    that case the SDK was defaulting to a broken configuration state, and
    failing silently. This is no longer the case, so any remaining cases of
    a misconfigured SDK are bugs that should be addressed, and are probably
    going unnoticed by users.
    
    Fixes #18, but at what cost?
    
    [specification]: https://opentelemetry.io/docs/specs/otel/error-handling/#basic-error-handling-principles

--- lib/OpenTelemetry/SDK.pm
+++ lib/OpenTelemetry/SDK.pm
@@ -5,18 +5,19 @@ our $VERSION = '0.025';
 
 use strict;
 use warnings;
-use experimental qw( signatures lexical_subs );
+use experimental qw( isa signatures lexical_subs );
 use feature 'state';
 
-use Module::Runtime;
 use Feature::Compat::Try;
+use Log::Any;
+use Module::Runtime;
 use OpenTelemetry::Common 'config';
 use OpenTelemetry::Propagator::Composite;
 use OpenTelemetry::SDK::Trace::TracerProvider;
-use OpenTelemetry;
+use OpenTelemetry::X;
 
 my sub configure_propagators {
-    my $logger = OpenTelemetry->logger;
+    my $logger = Log::Any->get_logger( category => 'OpenTelemetry' );
 
     state %map = (
         b3           => 'B3',
@@ -49,7 +50,9 @@ my sub configure_propagators {
             push @propagators, $class->new;
         }
         catch ($e) {
-            $logger->warnf("Error configuring '%s' propagator: %s", $name, $e);
+            die OpenTelemetry::X->create(
+                Invalid => "Error configuring '$name' propagator: $e",
+            );
         }
     }
 
@@ -58,7 +61,7 @@ my sub configure_propagators {
 }
 
 my sub configure_span_processors {
-    my $logger = OpenTelemetry->logger;
+    my $logger = Log::Any->get_logger( category => 'OpenTelemetry' );
 
     state %map = (
         jaeger  => '::Jaeger',
@@ -99,7 +102,9 @@ my sub configure_span_processors {
             );
         }
         catch ($e) {
-            $logger->warnf("Error configuring '%s' span processor: %s", $name, $e);
+            die OpenTelemetry::X->create(
+                Invalid => "Error configuring '$name' span processor: $e",
+            );
         }
     }
 
@@ -117,9 +122,9 @@ sub import ( $class ) {
         configure_span_processors();
     }
     catch ($e) {
-        OpenTelemetry->handle_error(
-            exception => $e,
-            message   => 'Unexpected configuration error'
+        die $e if $e isa OpenTelemetry::X;
+        die OpenTelemetry::X->create(
+            Invalid => "Unexpected error initialising OpenTelemetry::SDK: $e",
         );
     }
 }
